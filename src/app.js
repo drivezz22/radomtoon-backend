582a87d7c0b7931f2dac7f3aeb54a5938f9afe7c
PUMIN-L <puminbg@gmail.com>
2024-06-25 18:32:03 +0700

    feat:add payment

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const adminRouter = require("./routes/admin-route");
 const creatorAuthenticate = require("./middlewares/creator-authenticate");
 const commentRouter = require("./routes/comment-route");
 const milestoneRouter = require("./routes/milestone-route");
+;
 
 const app = express();
 
@@ -20,12 +21,54 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+
+
 app.use("/auth", authRouter);
 app.use("/admin", authenticate, adminAuthenticate, adminRouter);
 app.use("/creators", authenticate, creatorAuthenticate, creatorRouter);
 app.use("/products", authenticate, productRouter);
 app.use("/comments", authenticate, commentRouter);
 app.use("/milestones", authenticate, creatorAuthenticate, milestoneRouter);
+
+
+// feature/payment   ///////////////////////
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-08-01",
+});
+
+app.use(express.static(process.env.STATIC_DIR));
+
+
+app.get("/config", (req, res) => {
+    res.send({
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
+    });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            currency: "EUR",
+            amount: 1999,
+            automatic_payment_methods: { enabled: true },
+        });
+
+        // Send publishable key and PaymentIntent details to client
+        res.send({
+            clientSecret: paymentIntent.client_secret
+        });
+    } catch (e) {
+        return res.status(400).send({
+            error: {
+                message: e.message,
+            },
+        });
+    }
+});
+
+///////////////////////////////////
+
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);

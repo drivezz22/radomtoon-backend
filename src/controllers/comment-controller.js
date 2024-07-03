3e350351c4da194d03c5f565529a1f22dfda4857
drivezz22 <puwanut.pansailom@gmail.com>
2024-07-03 11:29:56 +0700

    fix-comment

--- src/controllers/comment-controller.js
+++ src/controllers/comment-controller.js
@@ -1,4 +1,4 @@
-const { APPROVAL_STATUS_ID } = require("../constants");
+const { APPROVAL_STATUS_ID, USER_ROLE } = require("../constants");
 const commentService = require("../services/comment-service");
 const productService = require("../services/product-service");
 const createError = require("../utils/create-error");
@@ -32,20 +32,53 @@ const validateProduct = (product) => {
 commentController.createComment = tryCatch(async (req, res) => {
   const { productId } = req.params;
   const { comment } = req.body;
-  const { id: userId } = req.user;
+  const { id: userId, role } = req.user;
 
   const existProduct = await productService.findProductById(+productId);
   validateProduct(existProduct);
 
-  const data = { userId, productId: +productId, comment };
+  let data;
+  if (role === USER_ROLE.SUPPORTER) {
+    data = { userId, productId: +productId, comment };
+  } else if (existProduct.creatorId === userId) {
+    data = { productId: +productId, comment };
+  }
+
+  if (!data) {
+    createError({
+      message: "Supporter and creator can comment",
+      statusCode: 400,
+    });
+  }
+
   const commentResult = await commentService.createComment(data);
 
-  res.status(201).json({ message: "Comment created", comment: commentResult });
+  const commentResultMap = [commentResult].map((el) => {
+    const commentData = {};
+    commentData.id = el.id;
+    commentData.userId = el.userId;
+    commentData.comment = el.comment;
+    commentData.productId = el.productId;
+    commentData.time = el.updatedAt;
+    commentData.creatorId = el.product.creatorId;
+    commentData.creatorFirstName = el.product.creator.firstName;
+    commentData.creatorLastName = el.product.creator.lastName;
+    commentData.creatorProfileImage = el.product.creator.profileImage;
+    if (el.user) {
+      commentData.supporterFirstName = el.user.firstName;
+      commentData.supporterLastName = el.user.lastName;
+      commentData.supporterProfileImage = el.user.profileImage;
+    }
+    return commentData;
+  });
+
+  res.status(201).json({ message: "Comment created", comment: commentResultMap[0] });
 });
 
 commentController.updateComment = tryCatch(async (req, res) => {
   const { commentId } = req.params;
   const { comment } = req.body;
+  const { id: userId, role } = req.user;
 
   const existComment = await commentService.findCommentById(+commentId);
   if (!existComment) {
@@ -56,10 +89,42 @@ commentController.updateComment = tryCatch(async (req, res) => {
   }
   validateProduct(existComment.product);
 
-  const data = { comment };
+  let data;
+  if (role === USER_ROLE.SUPPORTER) {
+    data = { userId, productId: existComment.productId, comment };
+  } else if (existComment.product.creatorId === userId) {
+    data = { productId: +existComment.productId, comment };
+  }
+
+  if (!data) {
+    createError({
+      message: "Supporter and creator can comment",
+      statusCode: 400,
+    });
+  }
+
   const commentResult = await commentService.updateCommentById(+commentId, data);
 
-  res.status(200).json({ message: "Comment updated", comment: commentResult });
+  const commentResultMap = [commentResult].map((el) => {
+    const commentData = {};
+    commentData.id = el.id;
+    commentData.userId = el.userId;
+    commentData.comment = el.comment;
+    commentData.productId = el.productId;
+    commentData.time = el.updatedAt;
+    commentData.creatorId = el.product.creatorId;
+    commentData.creatorFirstName = el.product.creator.firstName;
+    commentData.creatorLastName = el.product.creator.lastName;
+    commentData.creatorProfileImage = el.product.creator.profileImage;
+    if (el.user) {
+      commentData.supporterFirstName = el.user.firstName;
+      commentData.supporterLastName = el.user.lastName;
+      commentData.supporterProfileImage = el.user.profileImage;
+    }
+    return commentData;
+  });
+
+  res.status(200).json({ message: "Comment updated", comment: commentResultMap });
 });
 
 commentController.deleteComment = tryCatch(async (req, res) => {
@@ -78,7 +143,25 @@ commentController.getComment = tryCatch(async (req, res) => {
   const { productId } = req.params;
 
   const commentResultList = await commentService.getCommentByProductId(+productId);
-  res.status(200).json({ commentList: commentResultList });
+  const commentResultMap = commentResultList.map((el) => {
+    const commentData = {};
+    commentData.id = el.id;
+    commentData.userId = el.userId;
+    commentData.comment = el.comment;
+    commentData.productId = el.productId;
+    commentData.time = el.updatedAt;
+    commentData.creatorId = el.product.creatorId;
+    commentData.creatorFirstName = el.product.creator.firstName;
+    commentData.creatorLastName = el.product.creator.lastName;
+    commentData.creatorProfileImage = el.product.creator.profileImage;
+    if (el.user) {
+      commentData.supporterFirstName = el.user.firstName;
+      commentData.supporterLastName = el.user.lastName;
+      commentData.supporterProfileImage = el.user.profileImage;
+    }
+    return commentData;
+  });
+  res.status(200).json({ commentList: commentResultMap });
 });
 
 module.exports = commentController;

d71b2a00e2092e6878e319c6057f8a9acdab3c44
drivezz22 <puwanut.pansailom@gmail.com>
2024-07-06 21:34:57 +0700

    fix-stat

--- src/routes/stat-route.js
+++ src/routes/stat-route.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const creatorAuthenticate = require("../middlewares/creator-authenticate");
 const statController = require("../controllers/stat-controller");
+const authenticate = require("../middlewares/authenticate");
 
 const statRouter = express.Router();
 
 statRouter.get(
   "/product/:productId",
+  authenticate,
   creatorAuthenticate,
   statController.getStatByProduct
 );
@@ -15,18 +17,21 @@ statRouter.get("/", statController.getStat);
 
 statRouter.get(
   "/product/:productId/fund-trend",
+  authenticate,
   creatorAuthenticate,
   statController.getProductFundTrend
 );
 
 statRouter.get(
   "/product/:productId/tier-stat",
+  authenticate,
   creatorAuthenticate,
   statController.getTierStat
 );
 
 statRouter.get(
   "/product/:productId/map-density",
+  authenticate,
   creatorAuthenticate,
   statController.getMapDensityByProduct
 );
